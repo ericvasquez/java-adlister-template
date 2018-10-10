63b98856e13f4ed99e308ef641e75053cf7daff3
fer <fernandoza@gmail.com>
2018-10-10 14:26:03 -0500

    feat: finish the first part of the exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,9 +4,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        String name = request.getParameter("name");
+        String output = "World";
+
+        if(name != null){
+            output = name;
+        }
+
+        response.getWriter().println("<h1>Hello, "+output+"!</h1>");
+
     }
 }
