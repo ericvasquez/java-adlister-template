7b161dc3c8764576e02f49fee53a9dec516d79a2
fer <fernandoza@gmail.com>
2018-10-18 16:33:55 -0500

    feat: finish the passwords exercise

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,5 +1,6 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.Config;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
 
@@ -55,6 +56,7 @@ public class MySQLUsersDao implements Users {
         if (! rs.next()) {
             return null;
         }
+
         return new User(
             rs.getLong("id"),
             rs.getString("username"),
