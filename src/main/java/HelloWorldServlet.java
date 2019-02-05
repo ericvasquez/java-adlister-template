ef06f1f371f8ca012c1992ff94cbd5d44e144e29
ericvasquez <eric.vasquez3327@gmail.com>
2019-02-05 12:30:59 -0600

    first commit

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+//Create a page that displays a number that goes up by 1 every time the /count page is viewed.
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    private int counter = 0;
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+//    Allow the user to pass a parameter in the query string to reset the counter.
+        String reset = request.getParameter("reset");
+        if(reset == null) {
+            counter += 1;
+        } else {
+            counter = 0;
+        }
+        response.getWriter().println("<h1>The count is " + counter + ".</h1>");
     }
 }
