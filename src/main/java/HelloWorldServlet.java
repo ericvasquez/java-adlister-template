4457b1db414ca7478c709d87c9d0cf2bb767b352
ericvasquez <eric.vasquez3327@gmail.com>
2019-02-05 13:14:17 -0600

    added CounterServlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,20 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//Create a page that displays a number that goes up by 1 every time the /count page is viewed.
-@WebServlet(name = "CounterServlet", urlPatterns = "/count")
-public class CounterServlet extends HttpServlet {
-    private int counter = 0;
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-//    Allow the user to pass a parameter in the query string to reset the counter.
-        String reset = request.getParameter("reset");
-        if(reset == null) {
-            counter += 1;
-        } else {
-            counter = 0;
+//        Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
+        String name = request.getParameter("name");
+
+        if(name==null){
+            name = "World";
         }
-        response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+        out.println("<h1>Hello, " + name + " !</h1>");
     }
 }
